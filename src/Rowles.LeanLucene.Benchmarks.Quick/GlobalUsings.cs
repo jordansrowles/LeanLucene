08eb9a189d131e2917baa409de3b4ee0de70dc8b
jordansrowles <jordansrowles@gmail.com>
2026-04-14 01:22:41 +0100

    Remove Benchmarks.Quick, Benchmarks.RealData, old download script

--- src/Rowles.LeanLucene.Benchmarks.Quick/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using System.Diagnostics;
-global using System.Globalization;
-global using System.Runtime;
-global using System.Runtime.CompilerServices;
-global using System.Runtime.InteropServices;
-global using System.Text.Json;
